d5d122eee0201cb75fdc4deab311e361b6d9cede
Saleh EL Moukahal <121912565+SalehMk0@users.noreply.github.com>
2024-09-20 11:49:56 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ import mlflow.sklearn
 app = Flask(__name__)
 
 model = mlflow.sklearn.load_model(f"runs:/45b5223bc3c645daaf71a3c21765ca3c/model")
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get JSON data from request
+        
         data = request.json
         
-        # Convert JSON data to DataFrame
+        
         input_df = pd.DataFrame([data])
         
-        # Ensure input DataFrame matches the model's expected feature columns
         input_df = input_df[['age', 'sex', 'race', 'total_juvenile_offenses', 'detention_period', 'prior_offense_count', 'current_charge_degree']]
         print(input_df)
-        # Make prediction
+        
         input_df['age'] = input_df['age'].astype(int)
         input_df['sex'] = input_df['sex'].astype(int)
         input_df['race'] = input_df['race'].astype(int)
@@ -32,11 +34,11 @@ def predict():
         input_df['prior_offense_count'] = input_df['prior_offense_count'].astype(int)
         input_df['current_charge_degree'] = input_df['current_charge_degree'].astype(int)
         prediction = model.predict(input_df)
-        print(model)  # Log the model details
+        print(model)  
         print(prediction)
-        # Create response
+        
         response = {
-            'prediction': int(prediction[0]),  # Convert to int for JSON serialization
+            'prediction': int(prediction[0]), 
             'message': 'Success'
         }
         print(response)
